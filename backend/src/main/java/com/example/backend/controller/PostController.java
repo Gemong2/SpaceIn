c51e999aaa1d02f83152234333b45242322127bf
estar1996 <estar19961996@gmail.com>
2023-05-01 14:38:50 +0900

    feat: Post

--- backend/src/main/java/com/example/backend/controller/PostController.java
+++ backend/src/main/java/com/example/backend/controller/PostController.java
@@ -7,10 +7,9 @@ import com.example.backend.service.PostService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/posts")
@@ -24,4 +23,23 @@ public class PostController {
         PostResponseDto newPost = postService.createPost(postDto, postDto.getUserId());
         return new ResponseEntity<>(newPost, HttpStatus.CREATED);
     }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<PostResponseDto> getPost(@PathVariable Long id) {
+        PostResponseDto post = postService.getPost(id);
+        return new ResponseEntity<>(post, HttpStatus.OK);
+    }
+
+    @GetMapping("/{id}/near")
+    public ResponseEntity<List<PostResponseDto>> getNearbyPosts(@RequestParam("latitude") double userLatitude, @RequestParam("longitude") double userLongitude) {
+        List<PostResponseDto> nearbyPosts = postService.getNearbyPost(userLatitude, userLongitude, 5.0);
+        return new ResponseEntity<>(nearbyPosts, HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deletePost(@PathVariable Long id) {
+        postService.deletePost(id);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
 }
--- backend/src/main/java/com/example/backend/service/PostService.java
+++ backend/src/main/java/com/example/backend/service/PostService.java
@@ -11,6 +11,9 @@ import com.example.backend.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 @Service
 public class PostService {
 
@@ -31,4 +34,59 @@ public class PostService {
         postRepository.save(post);
         return PostResponseDto.fromEntity(post);
     }
+
+    public PostResponseDto getPost(Long id) {
+        Post post = postRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("Post not found with id" + id));
+        return PostResponseDto.fromEntity(post);
+    }
+
+    public List<PostResponseDto> getNearbyPost(double latitude, double longitude, double radiusInKm) {
+        List<Post> posts = postRepository.findAll();
+        return posts.stream()
+                .filter(post -> isWithinRadius(post.getPostLatitude(), post.getPostLongitude(), latitude, longitude, radiusInKm))
+                .map(PostResponseDto::fromEntity)
+                .collect(Collectors.toList());
+    }
+    private boolean isWithinRadius(Double postLatitude, Double postLongitude, double latitude, double longitude, double radiusInKm) {
+        double postLat = postLatitude;
+        double postLng = postLongitude;
+
+        final int EARTH_RADIUS = 6371;
+        double dLat = Math.toRadians(latitude - postLat);
+        double dLng = Math.toRadians(longitude - postLng);
+
+        double a = Math.sin(dLat / 2) * Math.sin(dLat / 2)
+                + Math.cos(Math.toRadians(postLat)) * Math.cos(Math.toRadians(latitude))
+                * Math.sin(dLng / 2) * Math.sin(dLng / 2);
+        double c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+        double distance = EARTH_RADIUS * c;
+
+        return distance <= radiusInKm;
+    }
+//    private boolean isWithinRadius(String postLatitude,String postLongitude, double latitude, double longitude, double radiusInKm) {
+//        double postLat = Double.parseDouble(postLatitude);
+//        double postLng = Double.parseDouble(postLongitude);
+//
+//        final int EARTH_RADIUS = 6371;
+//        double dLat = Math.toRadians(latitude - postLat);
+//        double dLng = Math.toRadians(longitude - postLng);
+//
+//        double a = Math.sin(dLat / 2) * Math.sin(dLat / 2)
+//                + Math.cos(Math.toRadians(postLat)) * Math.cos(Math.toRadians(latitude))
+//                * Math.sin(dLng / 2) * Math.sin(dLng / 2);
+//        double c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+//        double distance = EARTH_RADIUS * c;
+//
+//        return distance <= radiusInKm;
+//    }
+
+    public void deletePost(Long id) {
+        Post post = postRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("Post not found with id" + id));
+        postRepository.delete(post);
+    }
+
+
+
 }
